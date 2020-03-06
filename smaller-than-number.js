e91812a03b1f05f0c1631e279abee3fdcc008d87
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2020-03-05 22:21:33 -0500

    add more

--- smaller-than-number.js
+++ smaller-than-number.js
@@ -11,19 +11,20 @@ const smallerNumbersThanCurrentOriginal = (nums) => {
 };
 
 const smallerNumbersThanCurrent = (nums) => {
-    const sortedNums = nums.sort((a,b) => b - a);
+    const sortedNums = [...nums].sort((a,b) => b - a);
+    const orders = {};
+    for (let i = 0; i < sortedNums.length; i++) {
+        orders[sortedNums[i]] = sortedNums.length - i - 1;
+    }
 
-    const results = [];
-    // for (let i = 0; i < nums.length; i++) {
-    //     let count = 0;
-    //     for (let j = 0; j < nums.length; j++) {
-    //         if (nums[i] > nums[j]) count += 1;
-    //     }
-    //     results.push(count);
-    // }
-    // return results;
+    const finalNums = []
+    for (let i = 0; i < nums.length; i++) {
+        finalNums.push(orders[nums[i]])
+    }
+    return finalNums;
 };
 
+console.log((smallerNumbersThanCurrent([9,10,11,12])))  //[4,0,1,1,3]
 console.log((smallerNumbersThanCurrent([8,1,2,2,3])))  //[4,0,1,1,3]
 console.log((smallerNumbersThanCurrent([6,5,4,8])))  //[2,1,0,3]
 console.log((smallerNumbersThanCurrent([7,7,7,7])))  //[2,1,0,3]
